a88d799fa0e4aef6eb71dcb99e3e426781c49f1d
Franco Martin <mrfranco97@gmail.com>
2024-04-18 15:19:50 -0300

    Se adecuo el carrusel al formato react

--- TPO-API/src/main.jsx
+++ TPO-API/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import Navbar from './Navbar.jsx'
+import Carrusel from './Carrusel.jsx'
 //import './index.css'
 
 
@@ -9,5 +10,6 @@ import Navbar from './Navbar.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar/>
+    <Carrusel/>
   </React.StrictMode>,
 )
