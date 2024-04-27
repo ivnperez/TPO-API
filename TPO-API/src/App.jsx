c71e7a7d7883b0f7feae2d9bb8a6a65dcb8ea952
DESKTOP-1UO2SI0\mrfra <mrfranco97@gmail.com>
2024-04-27 19:58:20 -0300

    Se instalo la dependencia de ruteo

--- TPO-API/src/App.jsx
+++ TPO-API/src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
-import Navbar from './componentes/Navbar.jsx'
-import Banner from './componentes/Banner.jsx'
-import Servicios from './componentes/Servicios.jsx'
-import JuegosDestacados from './componentes/JuegosDestacados.jsx'
-import ConsolasDestacadas from './componentes/ConsolasDestacadas.jsx'
-
+import { useState } from "react";
+import Navbar from "./componentes/Navbar.jsx";
+import Banner from "./componentes/Banner.jsx";
+import Servicios from "./componentes/Servicios.jsx";
+import JuegosDestacados from "./componentes/JuegosDestacados.jsx";
+import ConsolasDestacadas from "./componentes/ConsolasDestacadas.jsx";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Banner/>
-    <Servicios/>
-    <JuegosDestacados/>
-    <ConsolasDestacadas/>
+      <Navbar />
+      <Banner />
+      <Servicios />
+      <JuegosDestacados />
+      <ConsolasDestacadas />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
