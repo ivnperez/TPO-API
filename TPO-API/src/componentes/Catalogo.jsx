88ee0963cf8daf55fe8877fc064d4bbdc0e42d8b
ivnperez <playgamer998@hotmail.com>
2024-05-01 18:05:35 -0300

    Se agrega paginacion al catalogo de producto y se agregan mejoras a los estilos de las cards

--- TPO-API/src/componentes/Catalogo.jsx
+++ TPO-API/src/componentes/Catalogo.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { getAll } from "../services/Juegos";
-import '/src/css/Catalago.css';
+import '/src/css/Catalogo.css';
 
 function Catalogo() {
     const [productos, setProductos] = useState([]);
+    const [paginaActual, setPaginaActual] = useState(1);
+    const productosPorPagina = 9;
 
     useEffect(() => {
         getAll()
@@ -16,22 +18,40 @@ function Catalogo() {
             });
     }, []);
 
+    const productosPaginaActual = productos.slice((paginaActual - 1) * productosPorPagina, paginaActual * productosPorPagina);
+
+    const cambiarPagina = (numeroPagina) => {
+        setPaginaActual(numeroPagina);
+    };
+
     return (
         <div className="catalogo-container">
             <div className="catalogo-grid">
-                {productos.map(product => (
-                    <div key={product.id} className="card" style={{ width: "18rem" }}>
+                {productosPaginaActual.map(product => (
+                    <div key={product.id} className="card">
                         <img src={product.imagen} className="card-img-top" alt="..."></img>
-                        <div className="card-body">
-                            <h5 className="card-title">{product.nombre}</h5>
-                            <p className="card-text">{product.precio}</p>
-                            <a href="#" className="btn btn-primary">Agregar al carrito</a>
+                        <div className="card-content">
+                            <div className="card-body">
+                                <h5 className="card-title">{product.nombre}</h5>
+                                <p className="card-text">{product.precio}</p>
+                            </div>
+                            <div className="card-footer">
+                                <a href="#" className="btn btn-primary">Agregar al carrito</a>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
+            <div className="pagination-container">
+                <div className="pagination">
+                    {[...Array(Math.ceil(productos.length / productosPorPagina)).keys()].map(numero => (
+                        <button key={numero + 1} onClick={() => cambiarPagina(numero + 1)}>{numero + 1}</button>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
+
 
 export default Catalogo;
