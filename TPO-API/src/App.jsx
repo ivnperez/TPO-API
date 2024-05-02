41b5e2972aeb62e6fc57ddacebb8efa694bbea61
Franco Martin <mrfranco97@gmail.com>
2024-05-02 16:30:37 -0300

    Se configuro navegacion en la navbar y componentes de navegacion

--- TPO-API/src/App.jsx
+++ TPO-API/src/App.jsx
@@ -1,23 +1,20 @@
 import Navbar from "./componentes/Navbar.jsx";
-import Banner from "./componentes/Banner.jsx";
-import Servicios from "./componentes/Servicios.jsx";
 import Catalogo from "./componentes/Catalogo.jsx";
-import ProductosDestacados from "./componentes/ProductosDestacados.jsx";
-import CarritoCompras from './componentes/CarritoCompras.jsx';
-
+import CarritoCompras from "./componentes/CarritoCompras.jsx";
+import { Route, Routes } from "react-router-dom";
+import Home from "./componentes/Home.jsx";
 
 function App() {
   return (
     <>
       <CarritoCompras>
-      <Navbar />
-      <Banner />
-      <Servicios />
-      <ProductosDestacados />
-      <Catalogo />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Catalogo" element={<Catalogo />} />
+        </Routes>
       </CarritoCompras>
     </>
   );
 }
 export default App;
-
