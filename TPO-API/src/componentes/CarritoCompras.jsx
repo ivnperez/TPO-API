3d4548406b9bcc8f65ca4e664020ccc946b54cd5
Luc4sRD <lucase.ruizdiaz@gmail.com>
2024-05-03 16:00:51 -0300

    Se agrega la primera version del checkout sin validar datos

--- TPO-API/src/componentes/CarritoCompras.jsx
+++ TPO-API/src/componentes/CarritoCompras.jsx
@@ -3,6 +3,7 @@ import React, { useState, createContext, useContext } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import "../css/CarritoCompras.css";
+
 const CarritoContext = createContext();
 
 // Hook UseContext
@@ -44,6 +45,7 @@ function CarritoCompras({ children }) {
         setCarrito([]);
     };
 
+    // Borro el carrito
     const borrarCarrito = () => {
         setCarrito([])
     };
@@ -85,7 +87,7 @@ function CarritoCompras({ children }) {
         <CarritoContext.Provider value={value}>
         {children}
         {/* aca arranca el primer modal */}
-        <div className="modal fade" id="carritoModal" tabIndex="-1" aria-labelledby="carritoModalLabel" aria-hidden="true">
+        <div className="modal fade" id="carritoModal" tabIndex="-1" data-bs-backdrop="false" aria-labelledby="carritoModalLabel" aria-hidden="true">
             <div className="modal-dialog modal-dialog-scrollable">
                 <div className="modal-content">
                     <div className="modal-header">
@@ -101,7 +103,7 @@ function CarritoCompras({ children }) {
                                 <p className="mb-0 texto-negro">{producto.nombre} </p>
                                 <p className="mb-0 texto-negro">{producto.precio}</p>
                             </div>
-                            {/* botones para agregar o disminuir cantidad de productos del carrito */}
+                            {/* botones para agregr o disminuir cantidad de productos del carrito */}
                             <div className="d-flex align-items-center me-2">
                             <button className="btn btn-warning me-2" onClick={() => decrementarCantidad(producto.nombre)}>
                                 <FontAwesomeIcon icon={faMinus} />
@@ -115,7 +117,7 @@ function CarritoCompras({ children }) {
                             <button className="btn btn-danger" onClick={() => eliminarDelCarrito(producto.nombre)}>
                                 <FontAwesomeIcon icon={faTrash} />
                             </button>
-                        </div>
+                            </div>
                         ))}
                     </div>
                     <div className="modal-footer">
@@ -123,24 +125,92 @@ function CarritoCompras({ children }) {
                     </div>
                     <div className="modal-footer texto-negro">
                         {/* Botones para confirmar/borrar carrito */}
-                        <button type="button" className="btn btn-danger" onClick={borrarCarrito} data-bs-dismiss="modal">Borrar Carrito</button>
-                        <button type="button" className="btn btn-success" onClick={borrarCarrito} data-bs-target="#confirmacionCompra" data-bs-toggle="modal">Comprar</button>
+                        <button type="button" className="btn btn-danger" onClick={borrarCarrito} data-bs-dismiss="modal" disabled={carrito.length === 0}>Borrar Carrito</button>
+                        <button type="button" className="btn btn-success" data-bs-target="#checkoutCompra" data-bs-toggle="modal" disabled={carrito.length === 0}>Comprar</button>
                     </div>
                 </div>
             </div>
         </div>
-        {/* aca arranca el segundo modal */}
-        <div className="modal fade" id="confirmacionCompra" aria-hidden="true" aria-labelledby="carritoModalLabel2" tabIndex="-1">
+
+
+
+        {/* aca arranca el Segundo modal */}
+        <div className="modal fade" id="checkoutCompra" data-bs-backdrop="static" aria-hidden="true" aria-labelledby="carritoModalLabel2" tabIndex="-1">
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title texto-negro" id="carritoModalLabel2">Checkout</h5>
+                    </div>
+                    <div className="modal-body">
+                    {/* Campo para ingresar nombre y apellido */}
+                    <div className="mb-3">
+                            <p className="texto-negro">Nombre y Apellido</p>
+                                <input type="text" className="form-control" id="nombre" placeholder="Nombre y Apellido" />
+                            </div>
+                            {/* Campo para ingresar dirección */}
+                            <div className="mb-3">
+                            <p className="texto-negro">Direccion</p>
+                                <input type="text" className="form-control" id="direccion" placeholder="Dirección" />
+                            </div>
+                            {/* Campo para ingresar código postal */}
+                            <div className="mb-3">
+                                <p className="texto-negro">Código Postal</p>
+                                <input type="text" className="form-control" id="codigoPostal" placeholder="Código Postal" />
+                            </div>
+                            {/* Campo para ingresar tarjeta */}
+                            <div className="mb-3">
+                                <p className="texto-negro">Número de Tarjeta</p>
+                                <input type="text" className="form-control" id="tarjeta" placeholder="Número de Tarjeta" />
+                            </div>
+                            {/* Campo para ingresar fecha de vencimiento */}
+                            <div className="mb-3">
+                                <p className="texto-negro">Fecha de Vencimiento</p>
+                                <input type="text" className="form-control" id="fechaVencimiento" placeholder="MM/AA" />
+                            </div>
+                            {/* Campo para ingresar CCV */}
+                            <div className="mb-3">
+                                <p className="texto-negro">CCV</p>
+                                <input type="text" className="form-control" id="ccv" placeholder="CCV" />
+                            </div>
+                            <hr className="my-4" />
+                            <div className='mb-3'>
+                                <p className="texto-negro">Productos</p>
+                            </div>    
+                    {Object.values(productosAgrupados).map((producto, index)  => (
+                            <div className="flex-grow-1" style={{ marginRight: '10px' }}>
+                                <p className="mb-0 texto-negro">{producto.nombre}  (X{producto.cantidad})</p>
+                                <p className="mb-0 texto-negro">{producto.precio}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="modal-footer">
+                        <p className="texto-negro"> Total: ${precioTotal.toLocaleString()}</p>
+                    </div>
+                    <div className="modal-footer texto-negro">
+                        {/*boton para el cartel de compra confirmada*/}
+                        <button type="button" className="btn btn-success" onClick={borrarCarrito} data-bs-target="#confirmacionCompra" data-bs-toggle="modal" disabled={carrito.length === 0}>Confirmar Compra</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        {/* aca arranca el Tercer modal */}
+        <div className="modal fade" id="confirmacionCompra" data-bs-backdrop="static" aria-hidden="true" aria-labelledby="carritoModalLabel2" tabIndex="-1">
             <div className="modal-dialog modal-dialog-centered">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title texto-negro" id="carritoModalLabel2">Confirmacion</h5>
                     </div>
                     <div className="modal-body">
-                    <p className="texto-negro">Su Compra ha sido Completada</p>
+                        <p className="texto-negro">Su Compra ha sido Completada</p>
                     </div>
                     <div className="modal-footer texto-negro">
-                    <button type="button" className="btn btn-success" onClick={redireccionar} data-bs-dismiss="modal">Volver al Inicio</button>
+                        {/*con este boton vuelvo a la pag de inicio despues de comrpar y borro el carrito */}
+                        <button type="button" className="btn btn-success" onClick={redireccionar} data-bs-dismiss="modal">Volver al Inicio</button>
                     </div>
                 </div>
             </div>
