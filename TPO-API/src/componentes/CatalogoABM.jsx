f58b1e44ac7d0aac20d58e9b3c6dc9c3d6cb427e
DESKTOP-1UO2SI0\mrfra <mrfranco97@gmail.com>
2024-05-02 22:07:05 -0300

    se agrego modal de agregado de productos

--- TPO-API/src/componentes/CatalogoABM.jsx
+++ TPO-API/src/componentes/CatalogoABM.jsx
@@ -104,7 +104,43 @@ function CatalogoABM() {
   const generarControlesFiltro = () => {
     return (
       <div>
-        <button className="btn btn-success mt-3">Agregar</button>
+        <button
+          className="btn btn-success mt-3"
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Agregar
+        </button>
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                  Agregar Producto
+                </h1>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">...</div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-success">
+                  Confirmar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
