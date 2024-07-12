5ebbeece511a7bbb76d9392565658009ff5668d1
MrFranco <50271790+mrfranco97@users.noreply.github.com>
2024-07-12 14:23:57 -0300

    Se agrego al main el detalle como nueva ruta

--- TPO-API/src/App.jsx
+++ TPO-API/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./componentes/Home.jsx";
 import Abm from "./componentes/Abm.jsx";
 import Registro from "./componentes/Registro.jsx";
+import DetalleProducto from "./componentes/DetalleProducto.jsx";
 import Login from "./componentes/Login.jsx";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Catalogo" element={<Catalogo />} />
+          <Route path="/Catalogo/:id" element={<DetalleProducto />} />
           <Route path="/Abm" element={<Abm />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/login" element={<Login />} />
--- TPO-API/src/componentes/DetalleProducto.jsx
+++ TPO-API/src/componentes/DetalleProducto.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchProductoByID } from "../features/productoSlice";
+import { agregarProducto } from "../features/carritoSlice";
 
-function DetalleProducto({ producto, onClose }) {
-    return (
-        <Modal show={true} onHide={onClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Detalles del Producto</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div style={{ textAlign: 'center' }}>
-                    {producto && <img src={producto.imagen} alt={producto.nombre} style={{ maxWidth: '100%', maxHeight: '400px' }} />}
-                </div>
-                <h3>{producto && producto.nombre}</h3>
-                <p>Descripción: {producto && producto.descripcion}</p>
-                <p>Precio: {producto && producto.precio}</p>
-                <p>Año de Lanzamiento: {producto && producto.lanzamiento}</p>
-                <p>Desarrollador: {producto && producto.desarrollador}</p>
-                {/* Agrega más campos según sea necesario */}
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={onClose}>
-                    Cerrar
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+function DetalleProducto() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const producto = useSelector((state) => state.productos.productoSeleccionado);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchProductoByID(Number(id)));
+    }
+  }, [dispatch, id]);
+
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        {producto && (
+          <img
+            src={producto.imagen}
+            alt={producto.nombre}
+            style={{ maxWidth: "100%", maxHeight: "400px" }}
+          />
+        )}
+      </div>
+      <h3>{producto && producto.nombre}</h3>
+      <p>Descripción: {producto && producto.descripcion}</p>
+      <p>Precio: {producto && producto.precio}</p>
+      <p>Año de Lanzamiento: {producto && producto.lanzamiento}</p>
+      <p>Desarrollador: {producto && producto.desarrollador}</p>
+      <button
+        className="btn btn-medium btn-black"
+        onClick={() => dispatch(agregarProducto(producto))}
+      >
+        Agregar al carrito
+      </button>
+    </>
+  );
 }
 
 export default DetalleProducto;
